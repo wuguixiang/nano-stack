a731147080922393b58a2dc6b9620acbc3556e4e
Wendy Goh <wgoh@hubspot.com>
2019-07-23 18:05:00 +0800

    add mongodb connection

--- app.js
+++ app.js
@@ -2,20 +2,48 @@ const express = require('express');
 //require a module
 const app = express();
 //just use a short variable
+
+const MongoClient = require('mongodb').MongoClient;
 var path = require('path')
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true}, (error,client) => {
+  if (error) throw error;
+
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("newcollection");
+  //you can change the collection name
+
+  //start the application after the database connection is ready
+  app.listen(port, () => {
+    console.log('This app is running on port' + port);
+  });
+});
+
+//use in all path
+//for form submission
+app.use(express.urlencoded({ extended: true}));
 
 
 //to use html and token
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
+const CONNECTION_URL = "mongodb+srv://wendyggx:Givelove148!@cluster0-alfjy.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "newdb"; //you can change the database name
+var database, collection;
+
 //this is a route
 app.get("/", (req, res, next) => {
   res.render('home', {page:'Home', menuId:'home'}); //rendering html template on the app page
 });
 
-app.post("/contact", (req, res, next) => {
-  res.send('hello world')
+app.post("/submit", (req, res, next) => {
+  //storing a submission into the database
+  collection.insertOne(req.body, (err,results) => {
+    if (err) return console.log(err)
+    console.log('saved to database');
+    res.send('hello world')
+  });
 });
 
 app.get("/contact", (req, res, next) => {
@@ -27,9 +55,6 @@ app.get("/about", (req, res, next) => {
   res.render('about', {page:'About', menuId:'about'}); //rendering html template on the app page
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('This app is running on port' + port);
-});
+
 //port in your computer for the app to run,
 //0-6500, 3000 not being use by system
