5ff8c48217e2758c8564426183bd173ae63e631a
Wendy Goh <wgoh@hubspot.com>
2019-08-12 18:35:17 +0800

    add oauth flow

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const split = require('split-string');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 // Supports a list of scopes as a string delimited by ',' or ' ' or '%20'
-const SCOPES = process.env.SCOPE.split('/ |, ?|%20/').join(' ');
+const SCOPES = process.env.SCOPE.split(/ |, ?|%20/).join(' ');
 
 const REDIRECT_URI = `http://localhost:${port}/oauth-callback`;
 //const REDIRECT_URI = `https://wendyggx.herokuapp.com/oauth-callback`;
